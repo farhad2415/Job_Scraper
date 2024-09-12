f2f643bdd64b88d24694892a0d224c1a8c7e758f
Md Farhad Hossain <farhad.lep@gmail.com>
2024-09-12 17:57:56 +0600

    Refactor scraper URLs and add category support

--- scraper/views.py
+++ scraper/views.py
@@ -1,7 +1,5 @@
-from urllib import request
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from .models import AvilableUrl, Job, Category
@@ -11,11 +9,6 @@ from django.core.paginator import Paginator
 from django.contrib import messages
 from selenium.common.exceptions import WebDriverException
 from webdriver_manager.chrome import ChromeDriverManager
-import time
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import ElementClickInterceptedException, TimeoutException
-
 def scrape_job_details(url, max_pages, category_slug):
     base_url = url.strip()
     category_slug = category_slug.strip()
@@ -33,110 +26,6 @@ def scrape_job_details(url, max_pages, category_slug):
         'total_skipped_jobs': 0
     }
     total_skipped_jobs = 0
-    if base_url == "https://jobsinmalta.com/jobs":
-        try:
-            for page_number in range(1 , max_pages + 1):
-                url = f"{base_url}/page:{page_number}/"
-                print(f"Scraping URL: {url}") 
-
-                driver.get(url)
-                if "No results found" in driver.page_source:
-                    print(f"No more pages found after page {page_number-1}.")
-                    break
-                job_grid_elements = driver.find_elements(By.CLASS_NAME, 'mobile-job-grid')
-                if job_grid_elements:
-                    for job_element in job_grid_elements:
-                        position = job_element.find_element(By.TAG_NAME, 'h2').text if job_element.find_element(By.TAG_NAME, 'h2') else "Position not found"
-                        company = job_element.find_element(By.CLASS_NAME, 'job-sub-title').text if job_element.find_element(By.CLASS_NAME, 'job-sub-title') else "Company not found"
-                        job_salary = job_element.find_element(By.CLASS_NAME, 'job-sub-details-title')[0].text if job_element.find_element(By.CLASS_NAME, 'job-sub-details-title')[0] else "Salary not found"
-                        job_type = job_element.find_element(By.CLASS_NAME, 'job-sub-details-title')[1].text if job_element.find_element(By.CLASS_NAME, 'job-sub-details-title')[1] else "Type not found"
-                        experience = job_element.find_element(By.CLASS_NAME, 'job-sub-details-title')[2].text if job_element.find_element(By.CLASS_NAME, 'job-sub-details-title')[2] else "Experience not found"
-                        print(f"Position: {position}, Company: {company}, Salary: {job_salary}, Type: {job_type}, Experience: {experience}")
-                        # scraped_data = Job(position=position, company=company)
-                        # scraped_data.save()
-                        if not Job.objects.filter(position=position, company=company, salary=job_salary).exists():
-                            scraped_data = Job(position=position, company=company, salary=job_salary)
-                            scraped_data.save()
-                        else:
-                            total_skipped_jobs += 1
-                        data = {
-                        "is_success": True,
-                        'url': url,
-                        'total_jobs_found': len(job_grid_elements),
-                        'total_skipped_jobs': total_skipped_jobs
-                        }
-                else:
-                    print(f"No job elements found on page {page_number}.")
-            time.sleep(15)
-            return data
-        # if an error occurs while permission denied
-        except Exception as e:
-            print(f"An error occurred: {e}")
-        finally:
-            driver.quit()
-
-    if base_url == "https://www.olx.ro/oferte/q-%C3%AEngrijitor-de-animale/":
-        data = {
-            "is_success": False,
-            'url': url,
-            'total_jobs_found': 0,
-            'total_skipped_jobs': 0
-        }
-        try:
-            for page_number in range(1, max_pages + 1):
-                url = f"{base_url}?page={page_number}"
-                print(f"Scraping URL: {url}")
-
-                driver.get(url)
-                page_source = driver.page_source
-                if "Nu am găsit niciun rezultat" in page_source:
-                    print(f"No more pages found after page {page_number-1}.")
-                    break
-                page_source = BeautifulSoup(page_source, 'html.parser')
-                job_grid_elements = page_source.find_all("div", class_='css-j0t2x2')
-                
-                if not job_grid_elements:
-                    print(f"No job elements found on page {page_number}.")
-                    continue
-                job_element = job_grid_elements[0]
-                data_processed = False
-                for index, content in enumerate(job_element.contents):
-                    if index % 4 == 0:  # Skip index 0, 4, 8, etc.
-                        continue
-                    
-                    job_position = content.find("h6").get_text(strip=True) if content.find("h6") else "Position not found"
-                    job_salary = content.find_all("p")[0].get_text(strip=True) if content.find("p") else "Salary not found"
-                    job_location = content.find_all("p")[1].get_text(strip=True) if len(content.find_all("p")) > 1 else "Location not found"
-                    job_type = content.find_all("p")[2].get_text(strip=True) if len(content.find_all("p")) > 2 else "Type not found"
-                        
-
-                    if not Job.objects.filter(position=job_position, salary=job_salary, location=job_location, job_type=job_type).exists():
-                        scraped_data = Job(position=job_position, salary=job_salary, location=job_location, job_type=job_type)
-                        scraped_data.save()
-                        data_processed = True  # Set flag to True if data is processed
-                    # Show a message if any data was processed
-                    if data_processed:
-                        print("Data has been processed and saved.")
-                    else:
-                        total_skipped_jobs += 1
-            data = {
-                "is_success": True,
-                'url': url,
-                'total_jobs_found': len(job_element),
-                'total_skipped_jobs': total_skipped_jobs
-            }
-            return data     
-        finally:
-            try:
-                driver.quit()
-            except WebDriverException as e:
-                print(f"WebDriverException: {e}")
-            except PermissionError as e:
-                print(f"PermissionError: {e}. Unable to terminate the WebDriver process.")
-            except Exception as e:
-                print(f"An unexpected error occurred while quitting the driver: {e}")
-
-
     if base_url == "https://www.romjob.ro/anunturi/locuri-de-munca/":
         data = {
             "is_success": False,
@@ -258,9 +147,13 @@ def scrape_view(request):
     paginator = Paginator(jobs, 20) 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
+    total_jobs = jobs.count()
 
-    return render(request, 'job_scraper.html')
-
+    return render(request, 
+                    'job_scraper.html', {
+                        'page_obj': page_obj, 
+                        'total_jobs': total_jobs
+                        })
 
 
 
