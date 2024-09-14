81d4135097c3703420aba01343754c7951cc9751
Md Farhad Hossain <farhad.lep@gmail.com>
2024-09-14 12:37:59 +0600

    Refactor scraper views.py, update ChromeDriver options, and fix time zone issue

--- scraper/views.py
+++ scraper/views.py
@@ -16,11 +16,20 @@ def scrape_job_details(url, max_pages, category_slug):
     category_slug = category_slug.strip()
     category_name = Category.objects.get(slug=category_slug).name
     # service = Service('/usr/bin/chromedriver')
-    service1 = Service(ChromeDriverManager().install())
-    options = Options()
-    options.add_argument('--headless') 
-    driver = webdriver.Chrome(service=service1, options=options)
+    # service1 = Service(ChromeDriverManager().install())
+    # options = Options()
+    # options.add_argument('--headless') 
+    # driver = webdriver.Chrome(service=service1, options=options)
    
+
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1920,1080')
+    options.add_argument('--remote-debugging-port=9222')
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)                                                                                                                                                
    
 
     data = {
